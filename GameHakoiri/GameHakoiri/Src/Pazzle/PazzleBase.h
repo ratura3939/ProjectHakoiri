40185951f23b4daf0dfb4eec57575b0efa91c687
ratura3939 <158240290+ratura3939@users.noreply.github.com>
2024-04-15 22:03:39 +0900

    いちよのベース

---
+++ GameHakoiri/GameHakoiri/Src/Pazzle/PazzleBase.h
@@ -0,0 +1,13 @@
+#pragma once
+class PazzleBase
+{
+public:
+	PazzleBase(void);	//コンストラクタ
+	virtual ~PazzleBase(void);	//デストラクタ
+
+	virtual bool Init(void);	//初期化
+	virtual void Update(void);	//更新
+	virtual void Draw(void);	//描画
+	virtual bool Release(void);	//解放
+};
+
